2b9298446b741c20ea0d03d3136771adabd512d3
temirlan <timtursunov@temirlans-MacBook-Pro.local>
2023-08-09 12:51:40 +0300

    add readme

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/su5kk/stix"
 )
 
 func main() {
+	// prepare data pipelines
 	addpipe := make(chan *stix.Task)
 	retrypipe := make(chan string)
-	scheduler := stix.NewScheduler(addpipe, retrypipe)
 	ctx := context.Background()
+
+	scheduler := stix.NewScheduler(addpipe, retrypipe)
 	shutdown := scheduler.Start(ctx)
+
+	// submit task
 	addpipe <- &stix.Task{
 		ID:          "1",
 		Name:        "my-task",
@@ -22,5 +27,10 @@ func main() {
 			return nil
 		},
 	}
+
+	// wait for execution
+	time.Sleep(1 * time.Second)
+
+	// shutdown scheduler
 	shutdown()
 }
